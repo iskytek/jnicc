875ffbc6a07da383f53f9d84365f623b7a692a42
Wolf <wolf@lehre-online.net>
2019-02-10 15:09:03 +0100

    +sample log and sol files

---
+++ run/sample.mps.sol
@@ -0,0 +1,14 @@
+Starting jni_coin.Main, Rel. 1.1.7, 2019-02-10 at Sun Feb 10 15:03:59 CET 2019...
+
+MPS file name is sample.mps
+Columns in lpSolver: 4, Rows in lpSolver: 4
+
+Setting number strong to 4...
+
+Solution is optimal. Objective value: 2.0
+
+x1=1.0
+x2=1.0
+
+
+***  END LOG FILE. Sun Feb 10 15:03:59 CET 2019  ***
