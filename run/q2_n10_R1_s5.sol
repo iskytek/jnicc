f04f46814448664f55da247c7848b994fa16e4d3
Wolf <wolf@lehre-online.net>
2019-02-18 14:30:40 +0100

    Diverse Anpassungen

--- run/q2_n10_R1_s5.sol
+++ run/q2_n10_R1_s5.sol
@@ -1,4 +1,4 @@
-Starting jni_coin.Main, Rel. 1.1.7, 2019-02-10 at Sun Feb 10 15:05:15 CET 2019...
+Starting jni_coin.Main, Rel. 1.1.7, 2019-02-10 at Sat Feb 16 13:55:21 CET 2019...
 
 Creating internal model using jni_coin.ModGen, Rel. 1.0.5, 2019-02-10
 Problem name is q2_n10_R1_s5
@@ -9,81 +9,57 @@ Upper bound Uj is RHS=32
 Setting number strong to 32...
 
 Solution found.
-x0=2 x1=4 x2=4 x3=3 x4=4 x5=2 x6=2 x7=5 x8=4 x9=3 x10=3 x11=3 x12=2 x13=4 x14=4 x15=2 x16=5 x17=4 x18=2 x19=4 
-x20=2 x21=4 x22=4 x23=2 x24=2 x25=3 x26=4 x27=3 x28=5 x29=2 x30=2 x31=4 
-NSO: Solutions found so far: 1. Best z so far: 103
+x0=3 x1=3 x2=3 x3=3 x4=3 x5=3 x6=3 x7=3 x8=3 x9=3 x10=3 x11=3 x12=3 x13=6 x14=3 x15=2 x16=3 x17=3 x18=3 x19=3 
+x20=3 x21=3 x22=3 x23=3 x24=3 x25=3 x26=3 x27=3 x28=3 x29=2 x30=3 x31=5 
+NSO: Solutions found so far: 2. Best z so far: 99
 -----------------------------------------------
 
 Solution found.
-x0=2 x1=4 x2=4 x3=3 x4=4 x5=2 x6=2 x7=5 x8=4 x9=3 x10=3 x11=3 x12=2 x13=5 x14=4 x15=2 x16=5 x17=4 x18=2 x19=3 
-x20=2 x21=4 x22=4 x23=2 x24=2 x25=3 x26=4 x27=3 x28=4 x29=2 x30=2 x31=4 
-NSO: Solutions found so far: 2. Best z so far: 102
------------------------------------------------
-
-Solution found.
-x0=1 x1=4 x2=4 x3=2 x4=4 x5=2 x6=2 x7=5 x8=10 x9=1 x10=1 x11=4 x12=1 x13=4 x14=4 x15=2 x16=4 x17=4 x18=2 x19=4 
-x20=2 x21=4 x22=4 x23=2 x24=1 x25=4 x26=4 x27=2 x28=4 x29=2 x30=2 x31=4 
-NSO: Solutions found so far: 3. Best z so far: 100
------------------------------------------------
-
-Solution found.
-x0=2 x1=4 x2=4 x3=2 x4=4 x5=2 x6=2 x7=5 x8=4 x9=2 x10=2 x11=4 x12=2 x13=4 x14=4 x15=2 x16=4 x17=4 x18=2 x19=4 
-x20=2 x21=4 x22=4 x23=2 x24=2 x25=4 x26=4 x27=2 x28=4 x29=2 x30=2 x31=4 
-NSO: Solutions found so far: 4. Best z so far: 99
------------------------------------------------
-
-Solution found.
-x0=2 x1=4 x2=4 x3=2 x4=4 x5=2 x6=2 x7=4 x8=4 x9=2 x10=2 x11=4 x12=2 x13=4 x14=4 x15=2 x16=4 x17=4 x18=2 x19=4 
-x20=2 x21=4 x22=4 x23=2 x24=2 x25=4 x26=4 x27=2 x28=4 x29=2 x30=2 x31=4 
-NSO: Solutions found so far: 5. Best z so far: 98
------------------------------------------------
-
-Solution found.
-x0=3 x1=3 x2=3 x3=3 x4=3 x5=3 x6=3 x7=3 x8=3 x9=3 x10=3 x11=3 x12=3 x13=3 x14=3 x15=3 x16=3 x17=4 x18=3 x19=3 
+x0=3 x1=3 x2=3 x3=3 x4=3 x5=3 x6=3 x7=3 x8=3 x9=3 x10=3 x11=3 x12=3 x13=4 x14=3 x15=3 x16=3 x17=3 x18=3 x19=3 
 x20=3 x21=3 x22=3 x23=3 x24=3 x25=3 x26=3 x27=3 x28=3 x29=3 x30=3 x31=3 
-NSO: Solutions found so far: 6. Best z so far: 97
+NSO: Solutions found so far: 4. Best z so far: 97
 -----------------------------------------------
 
 Solution found.
-x0=2 x1=4 x2=4 x3=2 x4=4 x5=2 x6=2 x7=4 x8=4 x9=2 x10=2 x11=4 x12=2 x13=4 x14=4 x15=2 x16=4 x17=2 x18=2 x19=4 
-x20=2 x21=4 x22=4 x23=2 x24=2 x25=4 x26=4 x27=2 x28=4 x29=2 x30=2 x31=4 
-NSO: Solutions found so far: 7. Best z so far: 96
+x0=3 x1=3 x2=3 x3=3 x4=3 x5=3 x6=3 x7=3 x8=3 x9=3 x10=3 x11=3 x12=3 x13=3 x14=3 x15=3 x16=3 x17=3 x18=3 x19=3 
+x20=3 x21=3 x22=3 x23=3 x24=3 x25=3 x26=3 x27=3 x28=3 x29=3 x30=3 x31=3 
+NSO: Solutions found so far: 6. Best z so far: 96
 -----------------------------------------------
 
 Solution is optimal. Objective value: 96.0
 
-x0=2.0
-x1=4.0
-x2=4.0
-x3=2.0
-x4=4.0
-x5=2.0
-x6=2.0
-x7=4.0
-x8=4.0
-x9=2.0
-x10=2.0
-x11=4.0
-x12=2.0
-x13=4.0
-x14=4.0
-x15=2.0
-x16=4.0
-x17=2.0
-x18=2.0
-x19=4.0
-x20=2.0
-x21=4.0
-x22=4.0
-x23=2.0
-x24=2.0
-x25=4.0
-x26=4.0
-x27=2.0
-x28=4.0
-x29=2.0
-x30=2.0
-x31=4.0
+x0=3.0
+x1=3.0
+x2=3.0
+x3=3.0
+x4=3.0
+x5=3.0
+x6=3.0
+x7=3.0
+x8=3.0
+x9=3.0
+x10=3.0
+x11=3.0
+x12=3.0
+x13=3.0
+x14=3.0
+x15=3.0
+x16=3.0
+x17=3.0
+x18=3.0
+x19=3.0
+x20=3.0
+x21=3.0
+x22=3.0
+x23=3.0
+x24=3.0
+x25=3.0
+x26=3.0
+x27=3.0
+x28=3.0
+x29=3.0
+x30=3.0
+x31=3.0
 
 
-***  END LOG FILE. Sun Feb 10 15:05:15 CET 2019  ***
+***  END LOG FILE. Sat Feb 16 13:55:22 CET 2019  ***
